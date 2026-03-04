6d39127a8f73045eb012dd1a6d66e00708a3d4e3
kawai-yuuki <20rd068@ms.dendai.ac.jp>
2026-03-04 17:01:42 +0900

    first commit

---
+++ BroCode_workspace/README.md
@@ -0,0 +1,1 @@
+# BroCode_workspace
---
+++ c/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    
+    int age = 25;
+    float price = 19.99;
+    double pi = 3.14159265358979;
+    char currency = '$';
+    char name[] = "Bro Code";
+
+    printf("%d\n", age);
+    printf("%f\n", price);
+    printf("%lf\n", pi);
+    printf("%c\n", currency);
+    printf("%s\n", name);
+
+    return 0;
+}
