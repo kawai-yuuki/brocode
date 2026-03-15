6de5a238a9143a9cd5d3528bfd571cd4934431ec
YukiKawai <20rd068@ms.dendai.ac.jp>
2026-03-15 21:56:54 +0900

    nya

---
+++ c/OurFirstProgram/src/test.c
@@ -0,0 +1,14 @@
+/* 
+Author: Yuki Kawai
+Purpose: This program prints out my name to the screen
+Date: 2026/3/14
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    // this code is displaying my name
+    printf("Hi, my name is Yuki\n");
+    return 0;
+}
